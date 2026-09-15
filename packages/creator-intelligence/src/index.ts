91b83cdccff8afe4eeff1a0fa9a135b7f2154323
Petrus <fabianmartens95@gmail.com>
2026-09-15 20:18:58 +0200

    feat: export creator intelligence core

---
+++ packages/creator-intelligence/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./creator";
+export * from "./matching-types";
+export { buildCreatorGraph, validateCreator } from "./validate";
+export { scoreCreatorPerformance } from "./performance";
+export { defaultMatchWeights, resolveMatchWeights } from "./weights";
+export { matchCreators } from "./matching";
