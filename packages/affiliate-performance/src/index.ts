664e4e6308b2eed351143b5ace6476b986825a2a
Petrus <fabianmartens95@gmail.com>
2026-09-16 01:32:24 +0200

    feat: export seller analytics adapter foundation

--- packages/affiliate-performance/src/index.ts
+++ packages/affiliate-performance/src/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./normalize.js";
 export * from "./aggregate.js";
 export * from "./tiktok-seller-analytics.js";
+export * from "./tiktok-seller-adapter.js";
 export * from "./csv.js";
