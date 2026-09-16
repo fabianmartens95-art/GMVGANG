9cc076fb2f0363f4004051a823ba827b8bb14911
Petrus <fabianmartens95@gmail.com>
2026-09-16 19:41:03 +0200

    feat: export referral reward repository

--- packages/platform-supabase/src/index.ts
+++ packages/platform-supabase/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./access.js";
 export * from "./client.js";
+export * from "./referral-rewards.js";
 export * from "./referrals.js";
 export * from "./registration.js";
 export * from "./session.js";
