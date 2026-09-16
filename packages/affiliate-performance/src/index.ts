4b777519afe629bbb44f0347f62efb7801f0a08d
Petrus <fabianmartens95@gmail.com>
2026-09-16 18:05:26 +0200

    feat: export affiliate performance read model

--- packages/affiliate-performance/src/index.ts
+++ packages/affiliate-performance/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types.js";
 export * from "./normalize.js";
 export * from "./aggregate.js";
+export * from "./read-model.js";
 export * from "./tiktok-seller-analytics.js";
 export * from "./tiktok-seller-adapter.js";
 export * from "./csv.js";
