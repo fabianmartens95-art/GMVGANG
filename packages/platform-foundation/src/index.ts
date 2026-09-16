4e4604849adda914e75bb08fdc9deffd398797cd
Petrus <fabianmartens95@gmail.com>
2026-09-16 02:37:21 +0200

    feat: export platform session resolver

--- packages/platform-foundation/src/index.ts
+++ packages/platform-foundation/src/index.ts
@@ -3,4 +3,5 @@ export * from "./connections.js";
 export * from "./identity.js";
 export * from "./profiles.js";
 export * from "./referrals.js";
+export * from "./session.js";
 export * from "./types.js";
