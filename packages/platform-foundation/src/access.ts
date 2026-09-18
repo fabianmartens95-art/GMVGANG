2eec488fd4253a12e6362f7b5df642df0cadf9c9
Petrus <fabianmartens95@gmail.com>
2026-09-18 23:27:41 +0200

    foundation: add products.manage capability on current main

--- packages/platform-foundation/src/access.ts
+++ packages/platform-foundation/src/access.ts
@@ -12,6 +12,7 @@ export type PlatformCapability =
   | "creators.manage"
   | "campaigns.read_all"
   | "campaigns.manage"
+  | "products.manage"
   | "approvals.manage"
   | "creator.self.read"
   | "creator.self.update"
@@ -36,6 +37,7 @@ const ROLE_CAPABILITIES: Record<PlatformUserRole, readonly PlatformCapability[]>
     "creators.manage",
     "campaigns.read_all",
     "campaigns.manage",
+    "products.manage",
     "approvals.manage",
     "creator.self.read",
     "creator.self.update",
@@ -58,6 +60,7 @@ const ROLE_CAPABILITIES: Record<PlatformUserRole, readonly PlatformCapability[]>
     "creators.manage",
     "campaigns.read_all",
     "campaigns.manage",
+    "products.manage",
     "approvals.manage",
   ],
   creator_manager: ["team.workspace.read", "creators.read_all", "creators.manage", "campaigns.read_all", "campaigns.manage"],
@@ -67,6 +70,7 @@ const ROLE_CAPABILITIES: Record<PlatformUserRole, readonly PlatformCapability[]>
     "brands.manage",
     "campaigns.read_all",
     "campaigns.manage",
+    "products.manage",
     "approvals.manage",
   ],
   closer: ["team.workspace.read", "brands.read_all", "campaigns.read_all"],
@@ -84,6 +88,7 @@ const ROLE_CAPABILITIES: Record<PlatformUserRole, readonly PlatformCapability[]>
     "brand.shop_connections.manage",
     "brand.campaigns.read",
     "brand.approvals.manage",
+    "products.manage",
   ],
 };
 
