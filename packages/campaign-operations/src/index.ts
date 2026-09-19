6ad737e2813e3902957a89b7f807b99b8ec9977e
Petrus <fabianmartens95@gmail.com>
2026-09-18 23:33:39 -0700

    W4: normalize Insight to Brief exports

--- packages/campaign-operations/src/index.ts
+++ packages/campaign-operations/src/index.ts
@@ -21,4 +21,5 @@ export {
 } from "./campaign";
 export { getCampaignActionQueue } from "./actions";
 export { summarizeCampaign } from "./summary";
-export { prepareBriefFromInsight } from "./insight-brief";\nexport type { CreativeInsight, InsightBriefDraft, InsightEvidenceClass, InsightMetric, InsightMetricKey, InsightSourceType, PrepareInsightBriefResult } from "./insight-brief";\n
+export { prepareBriefFromInsight } from "./insight-brief";
+export type { CreativeInsight, InsightBriefDraft, InsightEvidenceClass, InsightMetric, InsightMetricKey, InsightSourceType, PrepareInsightBriefResult } from "./insight-brief";
